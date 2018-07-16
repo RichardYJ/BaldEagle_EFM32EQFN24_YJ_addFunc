244ae776932657215cb5db4073a5efc78e004dc1
RichardYJ <yi_jian27@163.com>
2018-07-16 13:11:06 +0800

    BaldEagle_EFM32EQFN24_YJ_addFunc_20180312（SVN提交版本）

---
+++ BaldEagle_EFM32EQFN24_YJ_addFunc/BaldEagle_EFM32EQFN24/Keil 8051 v9.53 - Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := D:/WorkSpace/MyCode/SYNC/BaldEagle_EFM32EQFN24_YJ_addFunc/BaldEagle_EFM32EQFN24/Keil\ 8051\ v9.53\ -\ Debug/EFM8LB12F32E.lib
+
---
+++ BaldEagle_EFM32EQFN24_YJ_addFunc/SI/BaldEagle_EFM32EQFN24_YJ.si4project/Backup/EFM8LB1_SMBus_MasterMultibyte(4191).h
@@ -0,0 +1,63 @@
+
+#ifndef SMBUS_MASTER_MULTIBYTE_H_
+#define SMBUS_MASTER_MULTIBYTE_H_
+
+//-----------------------------------------------------------------------------
+// Pin Definitions
+//-----------------------------------------------------------------------------
+SI_SBIT (SDA, SFR_P0, 6);              
+SI_SBIT (SCL, SFR_P0, 7);              
+#if 0
+SI_SBIT (LED1, SFR_P1, 6);             
+#define LED_ON   0
+#define LED_OFF  1
+
+SI_SBIT (DISP_EN, SFR_P3, 4);          
+#define DISP_BC_DRIVEN   0             
+#define DISP_EFM8_DRIVEN 1             
+#endif
+//-----------------------------------------------------------------------------
+// Global CONSTANTS
+//-----------------------------------------------------------------------------
+
+#define  SYSCLK               72000000 
+
+#define  SMB_FREQUENCY           10000 
+
+#define  WRITE                    0x00 
+#define  READ                     0x01 
+
+
+#define  TARGET_ADDR               0x10 
+
+
+#define  SMB_MTSTA                0xE0
+#define  SMB_MTDB                 0xC0
+#define  SMB_MRDB                 0x80
+
+
+#define  NUM_BYTES_WR                4  
+#define  NUM_BYTES_RD                2 
+//-----------------------------------------------------------------------------
+// Global VARIABLES
+//-----------------------------------------------------------------------------
+
+
+
+extern uint8_t SMB_DATA_IN[NUM_BYTES_RD];
+
+extern uint8_t SMB_DATA_OUT[NUM_BYTES_WR];
+
+extern const uint8_t TARGET;                
+
+extern volatile bool SMB_BUSY;        
+extern volatile bool SMB_RW;          
+extern uint16_t NUM_ERRORS;           
+
+extern bool SMB0_I2C_MasterWrite(uint16_t RegAddr, uint16_t RegValue);
+extern uint16_t SMB0_I2C_MasterRead(uint16_t RegAddr);
+
+#endif // SMBUS_MASTER_MULTIBYTE_H_
+//-----------------------------------------------------------------------------
+// End Of File
+//-----------------------------------------------------------------------------
